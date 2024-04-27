6258d54b5314c3b109a8c8ecb0d40691e598a4bb
bhargav srirangam <baggusn@gmail.com>
2024-04-27 14:32:04 +0100

    added encoding header

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 # PYTHONSCRIPT:     main.py
 # ------------------------------------------------------------------------------
@@ -14,9 +16,37 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {__author__}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def print_word_with_stars(word):
+    for letter in word:
+        print("*" * len(letter), end=" ")
+    print()
+
+def print_megadeth_forever_pattern():
+    pattern = [
+        "******  *********   **       **   *********   **         **   ********** ",
+        "**   **    **      ****     ****   **      **** **       **   **         ",
+        "**    **   **     ** **   ** **   **     ** ** **     **    **         ",
+        "**    **   **    **  ** **   **   **    **   ** **   **     **         ",
+        "**   **    **   **    ***    **   **   **     ** ** **      *********  ",
+        "******     **  **********    **   **  ********** **   **    **         ",
+        "**         ** **        **   **   ** **        ** **     **  **         ",
+        "**         ****          **  **   ****          ** **       ** **         ",
+        "**         **              ** **     **          ** **        ** **       ",
+        "**         **               ***       **         ** **         ** *********"
+    ]
+
+    for row in pattern:
+        print(row)
+
+# Call the function to print the pattern
+print_megadeth_forever_pattern()
+
+
+# Example usage:
+#word = "example"
+#print_word_with_stars(word)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+   print_megadeth_forever_pattern()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
